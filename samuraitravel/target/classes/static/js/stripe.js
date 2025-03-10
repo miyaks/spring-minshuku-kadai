d5d5df826fc7fdc673eeefa9d3723bccbfc2f9f7
miyaks <105896227+miyaks@users.noreply.github.com>
2025-03-11 00:05:14 +0900

    Delete samuraitravel directory

--- samuraitravel/target/classes/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripe = Stripe('pk_test_51R0K9M4MXj8tlkHa7oSZKTveJvMykqdKb7AdJBXvozgbZuOmL4ZRPfd46H0CXG6oyiW4qxjBrhpV0u4m3tWvHMag001tlLIbWI');
-const paymentButton = document.querySelector('#paymentButton');
-
-paymentButton.addEventListener('click', () => {
- stripe.redirectToCheckout({
-   sessionId: sessionId
- })
-});
